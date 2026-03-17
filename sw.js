f78cf845d97e428359c64a39c623d75a2db172d7
guotang96-creator <guotang96@gmail.com>
2026-03-17 12:45:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
-const CACHE_NAME = "lottery-v25-cache";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.webmanifest"
-];
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
-self.addEventListener("install", event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
